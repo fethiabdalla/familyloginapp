f2a7a47e09678195e96d1eda1881cb0bab8a5791
Fethi Abdalla <67521116+fethiabdalla@users.noreply.github.com>
2022-12-10 09:13:40 -0500

    Create loginapp.py

---
+++ loginapp.py
@@ -0,0 +1,26 @@
+print("Welcome to my Login Application!")
+username = input("Username > ")
+password = input("Password > ")
+print()
+if username == "aida" and password == "Aid@#5":
+    print("Hello Aida the most outgoing one of the family! Don't let work consume your life got it!)
+    print()
+    print("I hope you have an amazing day!")
+elif username == "fethi" and password == "F3th1#4":
+    print("Yo Fethi my man what's up with you playboy! I wanted to remind you to not be too hard of yourself okay!")
+    print()
+    print("Enjoy your day my guy!")
+elif username == "timaj" and password == "Tim@j#3":
+    print("Hey T I'm glad that you're able to make it! I wanted to remind you as the anchor to enjoy your life!)
+    print()
+    print("Hope you have a lovely day! Also, the family really misses you and Nofil.")
+elif username == "saada" and password == "S@@d@#2":
+    print("Hello Saada I'm glad you are able to access this application! I wanted to remind you as well to soon retire from your job and relax you definitely deserve it!)
+    print()
+    print("Hopw you enjoy your day take care!")
+elif username == "mohammed" and password == "M0h@mm3d#1":
+    print("Hey there Mohammed welcome to this application! I wanted to remind you as well that soon you should retire since you work so hard for the family you definitely deserve to be more free in your life!)
+    print()
+    print("I hope you enjoy today and have a great one!")
+else:
+    print("You are not allowed to access this family's application since you aren't a part of the family!")
